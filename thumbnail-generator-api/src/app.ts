f4146543021429c4e1ed05769fa47700f28ed317
mauricio.paez <mauricio.paez@gensolve.com>
2022-04-27 10:09:16 -0500

    Use aws lambda types on the function

--- thumbnail-generator-api/src/app.ts
+++ thumbnail-generator-api/src/app.ts
@@ -1,6 +1,12 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
 export const lambdaHandler = async function (
-  event: any = {},
-  context: any
-): Promise<any> {
+  event: APIGatewayProxyEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
   return { statusCode: 201, body: "Hello from AWS CDK and SAM!" };
 };
