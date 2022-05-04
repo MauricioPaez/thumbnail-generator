9f76c9058dfcbd79944862bb408a168b5d617756
Mauricio Paez <mauriciol.paez@outlook.com>
2022-05-04 09:51:28 -0500

    Include logic to resize image and return getObject presigned Urls

--- thumbnail-generator-api/src/generate-thumbnails.ts
+++ thumbnail-generator-api/src/generate-thumbnails.ts
@@ -3,16 +3,15 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import AWS = require("aws-sdk");
+import { GetObjectRequest, PutObjectRequest } from "aws-sdk/clients/s3";
 import sharp = require("sharp");
+import Utils from "./utils";
 
-interface File {
-  name: string;
-  type: string;
-  size: number;
-}
-
-interface GenerateThumbnailsRequestModel {
-  file: File;
+interface GenerateThumbnailsResponseModel {
+  getObjectSignedUrl400x300: string;
+  getObjectSignedUrl160x120: string;
+  getObjectSignedUrl120x120: string;
 }
 
 export const lambdaHandler = async function (
@@ -20,37 +19,138 @@ export const lambdaHandler = async function (
   context: Context
 ): Promise<APIGatewayProxyResult> {
   console.log(event);
-  let resizedImg = null;
+  const s3Client = new AWS.S3({
+    signatureVersion: "v4",
+    region: process.env.REGION,
+  });
 
-  if (event.body) {
-    const requestModel: GenerateThumbnailsRequestModel = JSON.parse(event.body);
+  let getObjectSignedUrl400x300: string = "";
+  let getObjectSignedUrl160x120: string = "";
+  let getObjectSignedUrl120x120: string = "";
 
-    if (
-      requestModel.file.type !== "image/png" &&
-      requestModel.file.type !== "jpeg"
-    ) {
-      return { statusCode: 417, body: "File type must be PNG or JPEG" };
-    }
+  let fileS3Key = Utils.GetQueryParam("fileS3Key", event);
+  let fileName = Utils.GetQueryParam("fileName", event);
 
-    if (requestModel.file.size > 5000000) {
-      return { statusCode: 417, body: "File size exceeded the 5MB limit" };
-    }
-
-    resizedImg = await sharp(event.body).resize(400, 300).toFormat('png').toBuffer()
-
-  } else {
+  if (!fileS3Key) {
     return {
       statusCode: 400,
-      body: "Empty body. Please refer to API documentation",
+      body: "Missing [fileS3Key] query parameter",
     };
   }
 
+  if (!fileName) {
+    return {
+      statusCode: 400,
+      body: "Missing [fileName] query parameter",
+    };
+  }
+
+  try {
+    // Get original image
+    const params: GetObjectRequest = {
+      Bucket: process.env.UPLOADS_BUCKET_NAME || "",
+      Key: fileS3Key,
+    };
+
+    const image = await s3Client.getObject(params).promise();
+
+    if (!image.Body) {
+      console.log("Get object request failed", image);
+      return {
+        statusCode: 400,
+        body: "No object found with the provided key",
+      };
+    }
+
+    // Resize image
+    const buffer = Buffer.from(image.Body);
+    let resizedImg400x300 = await sharp(buffer, { failOnError: false })
+      .resize(400, 300)
+      .toFormat("png")
+      .toBuffer();
+    let resizedImg160x120 = await sharp(buffer, { failOnError: false })
+      .resize(160, 120)
+      .toFormat("png")
+      .toBuffer();
+    let resizedImg120x120 = await sharp(buffer, { failOnError: false })
+      .resize(120, 120)
+      .toFormat("png")
+      .toBuffer();
+
+    // Upload to S3
+    const key400x300: string = `400x300_${fileName}`;
+    let putObjectParams400x300: PutObjectRequest = {
+      Bucket: process.env.UPLOADS_BUCKET_NAME || "",
+      Key: key400x300,
+      Body: resizedImg400x300,
+    };
+    const key160x120: string = `160x120_${fileName}`;
+    let putObjectParams160x120: PutObjectRequest = {
+      Bucket: process.env.UPLOADS_BUCKET_NAME || "",
+      Key: key160x120,
+      Body: resizedImg160x120,
+    };
+    const key120x120: string = `120x120_${fileName}`;
+    let putObjectParams120x120: PutObjectRequest = {
+      Bucket: process.env.UPLOADS_BUCKET_NAME || "",
+      Key: key120x120,
+      Body: resizedImg120x120,
+    };
+
+    await s3Client.putObject(putObjectParams400x300).promise();
+    await s3Client.putObject(putObjectParams160x120).promise();
+    await s3Client.putObject(putObjectParams120x120).promise();
+
+    // Generate GetObject signedUrls
+    let getObjectParams400x300 = {
+      Bucket: process.env.UPLOADS_BUCKET_NAME,
+      Key: key400x300,
+      Expires: 300,
+    };
+    let getObjectParams160x120 = {
+      Bucket: process.env.UPLOADS_BUCKET_NAME,
+      Key: key160x120,
+      Expires: 300,
+    };
+    let getObjectParams120x120 = {
+      Bucket: process.env.UPLOADS_BUCKET_NAME,
+      Key: key120x120,
+      Expires: 300,
+    };
+
+    getObjectSignedUrl400x300 = s3Client.getSignedUrl(
+      "getObject",
+      getObjectParams400x300
+    );
+    getObjectSignedUrl160x120 = s3Client.getSignedUrl(
+      "getObject",
+      getObjectParams160x120
+    );
+    getObjectSignedUrl120x120 = s3Client.getSignedUrl(
+      "getObject",
+      getObjectParams120x120
+    );
+  } catch (error) {
+    console.log("Error", error);
+    return {
+      statusCode: 500,
+      body: "Server side error",
+    };
+  }
+
+  let response: GenerateThumbnailsResponseModel = {
+    getObjectSignedUrl400x300: getObjectSignedUrl400x300,
+    getObjectSignedUrl160x120: getObjectSignedUrl160x120,
+    getObjectSignedUrl120x120: getObjectSignedUrl120x120,
+  };
+
   const proxyResponse: APIGatewayProxyResult = {
     statusCode: 200,
-    body: resizedImg
-      ? Buffer.from(resizedImg).toString("base64")
-      : "Unable to transform file",
-    isBase64Encoded: true,
+    body: JSON.stringify(response),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+    },
   };
 
   return proxyResponse;
