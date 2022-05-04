34cf3a6fbdde1cda495b59f04712545fd0211610
Mauricio Paez <mauriciol.paez@outlook.com>
2022-05-04 10:22:54 -0500

    Update deployment script

--- thumbnail-generator-api/deploy.sh
+++ thumbnail-generator-api/deploy.sh
@@ -1,2 +1,13 @@
+# Build front end app before deploying
+cd ../thumbnail-generator-app
+echo "================> Building React App..."
 npm run build
+
+# Build NodeJs lambda functions before bundling
+cd ../thumbnail-generator-api
+echo "================> Build NodeJs functions..."
+npm run build
+
+# Deploy using AWS CDK CLI
+echo "================> Deploying stack..."
 cdk deploy --profile MPPersonal
